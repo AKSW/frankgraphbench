8388ffe257aa18fdf410aa9cd8397497bd95277d
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2023-06-05 21:47:16 -0300

    add dictionary mapping each dataset name with their corresponding class for Data Integration

---
+++ data_integration/dataset2class.py
@@ -0,0 +1,10 @@
+dataset2class = {
+    'MovieLens100k': {
+        'submodule': 'movielens',
+        'class': 'MovieLens100k'
+    },
+    'MovieLens1M': {
+        'submodule': 'movielens',
+        'class': 'MovieLens1M'
+    }
+}
