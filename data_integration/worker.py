d992d5f677544aaf4f99f1fee00527a8d5a80ff2
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2023-06-11 16:28:12 -0300

    add worker subclass derived from Thread

---
+++ data_integration/worker.py
@@ -0,0 +1,25 @@
+from threading import Thread
+import queue
+
+class Worker(Thread):
+    def __init__(self, request_queue, query_function, pbar=None):
+        super().__init__()
+        self.queue = request_queue
+        self.local_results = []
+        self.query = query_function
+        self.pbar = pbar
+    
+    def run(self):
+        while True:
+            try:
+                idx, params = self.queue.get(block=False)
+                response = self.query(params)
+                self.local_results.append((idx, response))
+                if self.pbar is not None:
+                    self.pbar.update(n=1)
+                self.queue.task_done()
+            except queue.Empty:
+                break
+            except Exception as e:
+                print(f'Exception:')
+                print(e)    
