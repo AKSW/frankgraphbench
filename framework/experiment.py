ceb6d2b9fbfd652064195559e4fbe9710e1a0469
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2023-09-19 11:22:28 -0300

    add overall evaluation report for all folds

--- framework/experiment.py
+++ framework/experiment.py
@@ -1,12 +1,13 @@
 import yaml
 import json
 import importlib
+from collections import defaultdict
 
 from .dataloader.dataloader import load, preprocess, split
 from .recommender.model2class import model2class
 from .evaluator.metric2class import metric2class
 
-import networkx as nx
+import numpy as np
 
 def _load_model_class(name):
     # relative path from root
@@ -44,7 +45,9 @@ def run(config_path):
         eval_metrics.append(metric_instance)
     
     # Loop over dataset (specially if its a k-fold split)
-    for dataset in split(G, **experiment['split']):
+    model_metrics = defaultdict(list)
+    for fold, dataset in enumerate(split(G, **experiment['split'])):
+        print(f'{fold+1}-Fold: evaluating model ...')
         print(G.info()) 
 
         for model_config in experiment['models']:
@@ -56,14 +59,29 @@ def run(config_path):
             # Training model
             G_train, ratings_train = dataset.get_train_data()
             print(f'Training model: {model.name()}...')
-            model.train(G_train, ratings_train)
 
+            model.train(G_train, ratings_train)
             # Getting recomendations
             # From this data I can evaluate the result for validation and test data
             recs = model.get_recommendations(k=k)
-            print(f'Evaluating model ...')
+            
+            metric_vals = []
             for metric in eval_metrics:
-                print(f'{metric.name()}: {metric.eval(ratings_train, recs)}')
+                metric_val = metric.eval(ratings_train, recs) 
+                print(f'{metric.name()}: {metric_val}')
+                metric_vals.append(metric_val)
+
+
+            model_metrics[model.name()].append(metric_vals) 
+    
+    print('----'*20)
+    print(f'Metrics final result')
+    for model, metrics in model_metrics.items():
+        print(f'{model}')
+        metrics_mean = np.array(metrics).mean(axis=0)
+        for idx, metric_mean in enumerate(metrics_mean):
+            print(f'\t{eval_metrics[idx].name()} mean: {metric_mean}')
+                
         
 
 
