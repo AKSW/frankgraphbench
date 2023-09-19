e58486a5a36643a05e629328553d340589b83a31
AlvaroJoseLopes <alvarojoselopes@hotmail.com>
2023-09-19 11:27:48 -0300

    remove filter by rating redundancy

--- framework/dataloader/preprocess/methods.py
+++ framework/dataloader/preprocess/methods.py
@@ -5,7 +5,6 @@ from tqdm import tqdm
 def apply_method(G, **info):
     method2fun = {
         'binarize': binarize,
-        'filter_by_rating': filter_by_rating,
         'filter_kcore': filter_kcore
     }
 
@@ -21,22 +20,13 @@ def binarize(G: Graph, threshold: int):
         edge_data = G.get_edge_data(user,item)
         
         bin_rate = 0 # negative
-        if edge_data['rating'] > threshold:
+        if edge_data['rating'] >= threshold:
             bin_rate = 1
 
         G[user][item]['rating'] = bin_rate
 
-def filter_by_rating(G: Graph, min: float, max: float):
-    desc=f'Filtering by rate, where min={min} and max={max}'
-    for (user, item) in tqdm(list(G.get_rating_edges()), desc=desc):
-        edge_data = G.get_edge_data(user,item)
-        
-        rating = edge_data['rating']
-        if not (rating >= min and rating <= max):
-            G.remove_edge(user, item)
-
-def filter_kcore(G: Graph, core: int, target: str, iterations: int = 1):
-    print(f'K-core filtering (target={target}), where core={core} and {iterations} it.')
+def filter_kcore(G: Graph, k: int, target: str, iterations: int = 1):
+    print(f'K-core filtering (target={target}), where core={k} and {iterations} it.')
     for it in range(iterations):
         desc = f'K-core filtering {it+1} iteration over edges from each {target}'
 
@@ -50,7 +40,7 @@ def filter_kcore(G: Graph, core: int, target: str, iterations: int = 1):
 
         count = 0
         for (u, vs) in tqdm(data.items(), total=n_total, desc=desc):
-            if len(vs) < core:
+            if len(vs) < k:
                 G.remove_node(u)
                 count += 1
         if count == 0:
