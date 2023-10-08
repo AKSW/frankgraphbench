fix train data leakage

framework/dataloader/edge_splitter/edge_splitter.py
@@ -80,7 +80,7 @@ class EdgeSplitter():
             self.G.remove_edge(u,v)
             ratings[u].append((v, data['rating']))
 
-        return deepcopy(self.G), ratings 
+        return self.G, ratings 
     
     def _random_by_ratio(self, p: float, level: str) -> np.array:
         if level == 'global':

framework/experiment.py
@@ -2,10 +2,12 @@ import yaml
 import json
 import importlib
 from collections import defaultdict
+from copy import deepcopy
 
 from .dataloader.dataloader import load, preprocess, split
 from .recommender.model2class import model2class
 from .evaluator.metric2class import metric2class
+from .reporter.report import Reporter
 
 import numpy as np
 
@@ -26,7 +28,6 @@ def run(config_path):
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
     
-    print(json.dumps(config, indent=4))
     experiment = config['experiment']
 
     G = load(**experiment['dataset'])
@@ -65,22 +66,32 @@ def run(config_path):
             # From this data I can evaluate the result for validation and test data
             recs = model.get_recommendations(k=k)
             
+            ratings_test = dataset.get_test_data()
             metric_vals = []
             for metric in eval_metrics:
-                metric_val = metric.eval(ratings_train, recs) 
+                metric_val = metric.eval(ratings_test, recs) 
                 print(f'{metric.name()}: {metric_val}')
                 metric_vals.append(metric_val)
 
 
+
             model_metrics[model.name()].append(metric_vals) 
+        
+        print('----'*20)
+        
     
-    print('----'*20)
     print(f'Metrics final result')
     for model, metrics in model_metrics.items():
         print(f'{model}')
         metrics_mean = np.array(metrics).mean(axis=0)
-        for idx, metric_mean in enumerate(metrics_mean):
-            print(f'\t{eval_metrics[idx].name()} mean: {metric_mean}')
+        metrics_std = np.array(metrics).std(axis=0)
+        for idx, (metric_mean, metric_std) in enumerate(zip(metrics_mean, metrics_std)):
+            print(f'\t{eval_metrics[idx].name()} mean: {metric_mean} +- {metric_std}')
+
+    
+    reporter = Reporter(experiment['report']['file'], eval_metrics)
+    reporter.report(model_metrics)
+
                 
         
 