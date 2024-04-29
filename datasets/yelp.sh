383d7c49174a83ce89e93b286b8c3fd288964fda
PauloRVdC <pauloviviurka4@gmail.com>
2024-04-29 11:23:21 +0200

    not possible to create sh for yelp dataset

--- datasets/yelp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# install yelp challenge dataset version 4
-wget https://www.kaggle.com/datasets/yelp-dataset/yelp-dataset/download?datasetVersionNumber=4 --output yelp-4.zip
-unzip yelp-4.zip -d lastfm/
-rm yelp-4.zip
