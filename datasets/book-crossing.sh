2005c174c0b0e8b39cad3b9ee28396626e5da1f4
PauloRVdC <pauloviviurka4@gmail.com>
2024-05-15 10:55:01 +0200

    modifications to the book crossing bash file

--- datasets/book-crossing.sh
+++ datasets/book-crossing.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # install Book Crossing
-wget http://www2.informatik.uni-freiburg.de/~cziegler/BX/BX-CSV-Dump.zip
+#curl <GET the download link from kaggle (https://www.kaggle.com/datasets/somnambwl/bookcrossing-dataset?resource=download)> --output BX-CSV-Dump.zip
 unzip BX-CSV-Dump.zip -d book-crossing/
 rm BX-CSV-Dump.zip
