adbc3720de2265972b49c11e50840fb1a90c34b4
PauloRVdC <pauloviviurka4@gmail.com>
2026-02-20 15:12:24 +0100

    mind small download and handle shell file updated

--- datasets/mind-small.sh
+++ datasets/mind-small.sh
@@ -2,4 +2,6 @@
 # install mind-small train dataset (https://msnews.github.io/)
 # curl <YOUR WGET LINK FROM MSNEWS FOR MIND SMALL TRAIN> --output mind-small.zip
 unzip mind-small.zip -d mind-small/
+mv mind-small/MINDsmall_train/*.tsv mind-small/
+rm -rf mind-small/MINDsmall_train
 rm mind-small.zip
